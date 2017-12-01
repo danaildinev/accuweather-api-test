d179a4cfe5866952b64b3e3bd82c2f1d7026eb21
Danail Dinev <danail_dd@windowslive.com>
2017-12-01 10:24:30 +0200

    changed to openweather

--- js/main.js
+++ js/main.js
@@ -1,57 +1,48 @@
 let currentCityId,
-    queryCity = "http://dataservice.accuweather.com/locations/v1/cities/search",
-    queryTemperature =
-        "http://dataservice.accuweather.com/forecasts/v1/daily/1day/",
-    queryCurrent = "http://dataservice.accuweather.com/currentconditions/v1/",
-    paramQ = "&q=",
-    paramApiKey = "?apikey=YN51uLqqABDTlu9BowlPMn4tFAYJm5sF",
-    paramMetric = "&metric=true",
-    paramDetails = "&details=true";
+    queryApi = "http://api.openweathermap.org/data/2.5/weather?",
+    paramCity = "&q=",
+    paramApiKey = "apikey=21ff54b0caff47e8fdd0410c4293e650",
+    paramMetric = "&units=metric",
+    paramDetails = "&details=true",
+
+    additionalLink = "https://openweathermap.org/city/",
+
+    windSpeed;
 
 function getCity(name) {
     $.ajax({
         method: "GET",
-        url: queryCity + paramApiKey + paramQ + name,
+        url: queryApi + paramApiKey + paramCity + name + paramMetric,
         success: function(result) {
-            getCurrentTemp(result[0].Key);
+            console.log(result);
+            prepareFields(result);
         }
     });
 }
 
-function getCurrentTemp(id) {
-    $.ajax({
-        method: "GET",
-        url: queryTemperature + id + paramApiKey + paramMetric,
-        success: function(result) {
-            console.log(result);
-            $(".temp").text(
-                "Min: " +
-                    result.DailyForecasts[0].Temperature.Minimum.Value +
-                    "°" +
-                    result.DailyForecasts[0].Temperature.Minimum.Unit +
-                    "/" +
-                    "Max: " +
-                    result.DailyForecasts[0].Temperature.Maximum.Value +
-                    "°" +
-                    result.DailyForecasts[0].Temperature.Maximum.Unit
-            );
-            $(".card-text").text(result.Headline.Text);
-            $(".additional-info").attr("href", result.Headline.Link);
-        }
-    });
-    $.ajax({
-        method: "GET",
-        url: queryCurrent + id + paramApiKey,
-        success: function(result) {
-            console.log(result);
-            $(".curr").append(
-                result[0].Temperature.Metric.Value +
-                    "°" +
-                    result[0].Temperature.Metric.Unit
-            );
-            $(".w-text").text(result[0].WeatherText);
-        }
-    });
+function prepareFields(result) {
+    $(".title").text(result.name);
+    $(".w-text").text(result.weather[0].main);
+    $(".curr").append(result.main.temp + "°C");
+    $(".temp").text(
+        "Min: " + result.main.temp_min +
+        "°C/Max: " + result.main.temp_max + "°C"
+    );
+    $(".additional-info").attr("href", additionalLink + result.name);
+
+    //windy things
+    windSpeed = result.wind.speed;
+    $(".wind").append(windSpeed + "m/s");
+    rotateTheWheel();
+
+    /*$(".card-text").text(result.Headline.Text);
+
+    */
+}
+
+function rotateTheWheel() {
+    let inversed = 50 / windSpeed;
+    $(".wind-icon").css('animation-duration', inversed + 's');
 }
 
 getCity("plovdiv");
