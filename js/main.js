77beab43436dce674cca2534196737bb40c21a73
Danail Dinev <danail_dd@windowslive.com>
2017-12-02 00:18:01 +0200

    done

--- js/main.js
+++ js/main.js
@@ -1,32 +1,52 @@
-var currentCityId,
+//TODO: getCity -> prepare data -> create new card with the data/rotate the wheel; events
+
+let currentCityId,
     queryApi = "http://api.openweathermap.org/data/2.5/weather?",
-    queryApi5day = "http://api.openweathermap.org/data/2.5/weather?",
+    queryApiHourly = "http://api.openweathermap.org/data/2.5/forecast?",
     paramCity = "&q=",
     paramApiKey = "apikey=21ff54b0caff47e8fdd0410c4293e650",
     paramMetric = "&units=metric",
     paramDetails = "&details=true",
-    additionalLink = "https://openweathermap.org/city/";
+    additionalLink = "https://openweathermap.org/city/",
+    tempSymbol = "°C",
+    isGrid = true;
 
 function getCity(name) {
-    $.ajax({
+    ajax1 = $.ajax({
         method: "GET",
         url: queryApi + paramApiKey + paramCity + name + paramMetric,
         success: function(result) {
-            console.log(result);
-            prepareCard(result);
+            resultCurrent = result;
         }
+    });
+
+    ajax2 = $.ajax({
+        method: "GET",
+        url: queryApiHourly + paramApiKey + paramCity + name + paramMetric,
+        success: function(result) {
+            resultHourly = result;
+        }
+    });
+
+    //stackoverflow.com/questions/33871061/wait-for-execution-of-two-ajax-requests-success-functions-in
+    $.when(
+        ajax1, ajax2
+    )
+    .done(function() {
+        prepareCard(resultCurrent, resultHourly);
+    })
+    .fail(function() {
+        console.log('api fail');
     });
 }
 
-function prepareCard(result) {
+function prepareCard(result, hourly) {
     cityName = result.name;
     currentWeatherText = result.weather[0].main;
-    currentTemperature = result.main.temp + "°C";
-    currentLoHi =
-        "Lo: " + result.main.temp_min + "°C/Hi: " + result.main.temp_max + "°C";
+    currentTemperature = result.main.temp + tempSymbol;
+    currentLoHi = "Lo: " + result.main.temp_min + tempSymbol + "/Hi: " + result.main.temp_max + tempSymbol;
     windSpeed = result.wind.speed;
     icon = result.weather[0].icon;
-    console.log(cityName);
 
     //create new card
     newCard(
@@ -36,10 +56,10 @@ function prepareCard(result) {
         currentLoHi,
         windSpeed,
         additionalLink,
-        icon
+        icon,
+        hourly
     );
-
-    rotateTheWheel(5);
+    rotateTheWheel(windSpeed);
 }
 
 function rotateTheWheel(windSpeed) {
@@ -54,10 +74,33 @@ function newCard(
     currentLoHi,
     windSpeed,
     additionalLink,
-    icon
+    icon,
+    hourly
 ) {
+    //DETAILS
+    var extra = "";
+    if (extraInfo) {
+        extra = `
+            <div class="card-footer bg-transparent">
+                <table class="table table-sm">
+                  <tbody>
+                    <tr><td>` + hourly.list[0].dt_txt + `</td><td>` + hourly.list[0].main.temp + tempSymbol + `</td>` +
+                    `<td>` + hourly.list[0].weather[0].main + `</td><td><img src="http://openweathermap.org/img/w/` + hourly.list[0].weather[0].icon + `.png"></td></tr>` +
+                    `<tr><td>` + hourly.list[1].dt_txt + `</td><td>` + hourly.list[1].main.temp + tempSymbol + `</td>` +
+                    `<td>` + hourly.list[1].weather[0].main + `</td><td><img src="http://openweathermap.org/img/w/` + hourly.list[1].weather[0].icon + `.png"></td></tr>` +
+                    `<tr><td>` + hourly.list[2].dt_txt + `</td><td>` + hourly.list[2].main.temp + tempSymbol + `</td>` +
+                    `<td>` + hourly.list[2].weather[0].main + `</td><td><img src="http://openweathermap.org/img/w/` + hourly.list[2].weather[0].icon + `.png"></td></tr>` +
+                    `<tr><td>` + hourly.list[3].dt_txt + `</td><td>` + hourly.list[3].main.temp + tempSymbol + `</td>` +
+                    `<td>` + hourly.list[3].weather[0].main + `</td><td><img src="http://openweathermap.org/img/w/` + hourly.list[3].weather[0].icon + `.png"></td></tr>` +
+                    `<tr><td>` + hourly.list[4].dt_txt + `</td><td>` + hourly.list[4].main.temp + tempSymbol + `</td>` +
+                    `<td>` + hourly.list[4].weather[0].main + `</td><td><img src="http://openweathermap.org/img/w/` + hourly.list[4].weather[0].icon + `.png"></td></tr>` +
+                  `</tbody>
+                </table>
+            </div>`;
+    }
+    //CARD DEFAULT
     $("#cards-container").append(
-        `<div class="col-md-5 card card-custom">
+        `<div class="col-md-5 card card-custom ${(isGrid)?"":"list-view"}">
             <div class="content">
                 <div class="left">
                     <div class="card-body">
@@ -69,8 +112,7 @@ function newCard(
                             <p class="wind">Wind: ${windSpeed}m/s</p>
                             <p class="card-text"></p>
                             <p class="card-link">
-                                <a class="additional-info" href="${additionalLink +
-                                    cityName}">Additional info</a>
+                                <a class="additional-info" href="${additionalLink + cityName}">Additional info</a>
                             </p>
                         </div>
                     </div>
@@ -82,18 +124,30 @@ function newCard(
                     <i class="fa fa-star wind-icon" aria-hidden="true"></i>
                     <i class="fa fa-long-arrow-up wind-body" aria-hidden="true"></i>
                 </div>
-            </div>
-            <div class="card-footer bg-transparent border-success">
-                Footer
-            </div>
-        </div>`
-    );
+            </div> ` + extra + `
+        </div>`);
 }
-
-getCity("plovdiv");
 
 /* events */
 $("#searchButton").click(function() {
     selectedCity = $("#searchInput").val();
+    extraInfo = $("#extraInfo").is(":checked");
     getCity(selectedCity);
 });
+
+$('.removeCity').on('click', function() {
+    alert($(this).closest("div.col-md-5"));
+    $(this).closest("div.col-md-5").remove();
+});
+
+$('#gridRadio').click(function() {
+    $("#cards-container>div").toggleClass("list-view");
+    isGrid = true;
+});
+
+$('#listRadio').click(function() {
+    $("#cards-container>div").toggleClass("list-view");
+    isGrid = false;
+});
+
+getCity("plovdiv");
