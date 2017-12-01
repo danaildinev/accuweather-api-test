08f661aacbe6681f2a33ac917981974a6d2d37ca
dinevd <parolchica1>
2017-12-01 18:23:48 +0200

    search is now working

--- js/main.js
+++ js/main.js
@@ -1,13 +1,11 @@
-let currentCityId,
+var currentCityId,
     queryApi = "http://api.openweathermap.org/data/2.5/weather?",
+    queryApi5day = "http://api.openweathermap.org/data/2.5/weather?",
     paramCity = "&q=",
     paramApiKey = "apikey=21ff54b0caff47e8fdd0410c4293e650",
     paramMetric = "&units=metric",
     paramDetails = "&details=true",
-
-    additionalLink = "https://openweathermap.org/city/",
-
-    windSpeed;
+    additionalLink = "https://openweathermap.org/city/";
 
 function getCity(name) {
     $.ajax({
@@ -15,34 +13,87 @@ function getCity(name) {
         url: queryApi + paramApiKey + paramCity + name + paramMetric,
         success: function(result) {
             console.log(result);
-            prepareFields(result);
+            prepareCard(result);
         }
     });
 }
 
-function prepareFields(result) {
-    $(".title").text(result.name);
-    $(".w-text").text(result.weather[0].main);
-    $(".curr").append(result.main.temp + "°C");
-    $(".temp").text(
-        "Min: " + result.main.temp_min +
-        "°C/Max: " + result.main.temp_max + "°C"
-    );
-    $(".additional-info").attr("href", additionalLink + result.name);
-
-    //windy things
+function prepareCard(result) {
+    cityName = result.name;
+    currentWeatherText = result.weather[0].main;
+    currentTemperature = result.main.temp + "°C";
+    currentLoHi =
+        "Lo: " + result.main.temp_min + "°C/Hi: " + result.main.temp_max + "°C";
     windSpeed = result.wind.speed;
-    $(".wind").append(windSpeed + "m/s");
-    rotateTheWheel();
+    icon = result.weather[0].icon;
+    console.log(cityName);
 
-    /*$(".card-text").text(result.Headline.Text);
+    //create new card
+    newCard(
+        cityName,
+        currentWeatherText,
+        currentTemperature,
+        currentLoHi,
+        windSpeed,
+        additionalLink,
+        icon
+    );
 
-    */
+    rotateTheWheel(5);
 }
 
-function rotateTheWheel() {
-    let inversed = 50 / windSpeed;
-    $(".wind-icon").css('animation-duration', inversed + 's');
+function rotateTheWheel(windSpeed) {
+    let inversed = 20 / windSpeed;
+    $(".wind-icon").css("animation-duration", inversed + "s");
+}
+
+function newCard(
+    cityName,
+    currentWeatherText,
+    currentTemperature,
+    currentLoHi,
+    windSpeed,
+    additionalLink,
+    icon
+) {
+    $("#cards-container").append(
+        `<div class="col-md-5 card card-custom">
+            <div class="content">
+                <div class="left">
+                    <div class="card-body">
+                        <h2 class="title">${cityName}</h2>
+                        <h3 class="curr">Current: ${currentTemperature}</h3>
+                        <h4 class="temp">${currentLoHi}</h4>
+                        <h5 class="text-muted w-text">${currentWeatherText}</h5>
+                        <div class="info">
+                            <p class="wind">Wind: ${windSpeed}m/s</p>
+                            <p class="card-text"></p>
+                            <p class="card-link">
+                                <a class="additional-info" href="${additionalLink +
+                                    cityName}">Additional info</a>
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="right">
+                    <img class="card-image" src="http://openweathermap.org/img/w/${
+                        icon
+                    }.png">
+                    <i class="fa fa-star wind-icon" aria-hidden="true"></i>
+                    <i class="fa fa-long-arrow-up wind-body" aria-hidden="true"></i>
+                </div>
+            </div>
+            <div class="card-footer bg-transparent border-success">
+                Footer
+            </div>
+        </div>`
+    );
 }
 
 getCity("plovdiv");
+
+/* events */
+$("#searchButton").click(function() {
+    selectedCity = $("#searchInput").val();
+    getCity(selectedCity);
+});
